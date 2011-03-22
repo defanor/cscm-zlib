98072900529904e7a89a5cda0a61cbe8f46d0045
joseph.gay <joseph.gay@fca3e652-9b03-0410-8d7b-ac86a6ce46c4>
2011-03-22 02:27:28 +0000

    test now uses test egg
    git-svn-id: https://code.call-cc.org/svn/chicken-eggs/release/4/zlib/trunk@23173 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- tests/run.scm
+++ tests/run.scm
@@ -1,26 +1,38 @@
-;; test for idempotence
+
+(import chicken scheme)
+(use ports simple-sha1 srfi-1 test zlib)
 
 (define in-data-size #x20000) ; 128K of data
 
-(import chicken scheme)
-(use ports simple-sha1 srfi-1 zlib)
+(define in-data (string-unfold null? car cdr (list-tabulate in-data-size (lambda (i) (integer->char (random 255))))))
+(define sha1 (string->sha1sum in-data))
 
-(let* ((in-data (string-unfold null? car cdr (list-tabulate in-data-size (lambda (i) (integer->char (random 255))))))
-       (sha1 (string->sha1sum in-data))
-       (out-data
-         (let ((string-port (open-output-string)))
-           (with-output-to-port (open-zlib-compressed-output-port string-port)
-             (lambda ()
-               (write-string in-data)
-               (close-output-port (current-output-port))
-               (get-output-string string-port))))))
-  (unless (string= sha1
-                   (string->sha1sum
-                    (let ((string-port (open-output-string)))
-                      (with-input-from-port (open-zlib-compressed-input-port (open-input-string out-data))
-                        (lambda ()
-                          (write-string (read-string) #f string-port)
-                          (close-output-port (current-output-port))
-                          (close-input-port (current-input-port))
-                          (get-output-string string-port))))))
-    (error "compression integrity check failed, sha1 does not match")))
+(define out-data "")
+
+;; test for idempotence
+
+(test-begin)
+
+(test-assert "deflate"
+             (begin
+               (set! out-data
+                     (let ((string-port (open-output-string)))
+                       (with-output-to-port (open-zlib-compressed-output-port string-port)
+                         (lambda ()
+                           (write-string in-data)
+                           (close-output-port (current-output-port))
+                           (get-output-string string-port)))))
+               (> (string-length out-data) 0)))
+
+(test "inflate" sha1
+      (string->sha1sum
+       (let ((string-port (open-output-string)))
+         (with-input-from-port (open-zlib-compressed-input-port (open-input-string out-data))
+           (lambda ()
+             (write-string (read-string) #f string-port)
+             (close-input-port (current-input-port))
+             (get-output-string string-port))))))
+
+(test-end)
+
+(test-exit)
